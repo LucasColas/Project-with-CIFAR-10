be38851580f30fadcde1c378f2401f2ced22a881
LucasColas <lucas.colas13@gmail.com>
2021-06-02 19:38:56 +0200

    update

--- main.py
+++ main.py
@@ -29,3 +29,23 @@ for file in names:
         plt.imshow(img_render, interpolation='bicubic')
         plt.show()
         break
+
+i = 0
+for file in names:
+    if i >= 9:
+        break
+    if "data" in file:
+        i += 1
+        path_file = os.path.join(files_path, file)
+        print("yes, data : ", file)
+        data = unpickle(path_file)
+        plt.subplot(330 + 1 + i)
+        img = data[b'data'][i]
+        img_r = img[0:1024].reshape(32,32)
+        img_g = img[1024:2048].reshape(32,32)
+        img_b = img[2048:].reshape(32,32)
+        img_render = np.dstack((img_r, img_g, img_b))
+        plt.imshow(img_render, interpolation='bicubic')
+        print(i)
+
+plt.show()
