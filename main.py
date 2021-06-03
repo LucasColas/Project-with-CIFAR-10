ec623b7067ea4059287851741f8c30a05cc872a0
LucasColas <lucas.colas13@gmail.com>
2021-06-03 13:45:50 +0200

    update

--- main.py
+++ main.py
@@ -14,22 +14,23 @@ def unpickle(file):
 names = os.listdir(files_path)
 print(names)
 
-for file in names:
-    if "data" in file:
-        path_file = os.path.join(files_path, file)
-        print("yes, data : ", file)
-        data = unpickle(path_file)
-        print(len(data))
-        print(data.keys())
-        img = data[b'data'][900]
-        img_r = img[0:1024].reshape(32,32)
-        img_g = img[1024:2048].reshape(32,32)
-        img_b = img[2048:].reshape(32,32)
-        img_render = np.dstack((img_r, img_g, img_b))
-        plt.imshow(img_render, interpolation='bicubic')
-        plt.imshow(img_render)
-        #plt.show()
-        break
+def render_one_image(file, i):
+    for file in names:
+        if "data" in file:
+            path_file = os.path.join(files_path, file)
+            print("yes, data : ", file)
+            data = unpickle(path_file)
+            print(len(data))
+            print(data.keys())
+            img = data[b'data'][i]
+            img_r = img[0:1024].reshape(32,32)
+            img_g = img[1024:2048].reshape(32,32)
+            img_b = img[2048:].reshape(32,32)
+            img_render = np.dstack((img_r, img_g, img_b))
+            plt.imshow(img_render, interpolation='bicubic')
+            plt.imshow(img_render)
+            #plt.show()
+            break
 
 def render_several_images(file, num):
     for file in names:
