9ae0c0a44c9566c755d0c2181d9d60539346e40b
LucasColas <lucas.colas13@gmail.com>
2021-06-03 13:44:05 +0200

    update

--- main.py
+++ main.py
@@ -31,22 +31,22 @@ for file in names:
         #plt.show()
         break
 
+def render_several_images(file, num):
+    for file in names:
 
-for file in names:
+        if "data" in file:
+            for i in range(num):
+                path_file = os.path.join(files_path, file)
+                print("yes, data : ", file)
+                data = unpickle(path_file)
+                plt.subplot(330 + 1 + i)
+                img = data[b'data'][i]
+                img_r = img[0:1024].reshape(32,32)
+                img_g = img[1024:2048].reshape(32,32)
+                img_b = img[2048:].reshape(32,32)
+                img_render = np.dstack((img_r, img_g, img_b))
+                plt.imshow(img_render)
+                print(i)
+            break
 
-    if "data" in file:
-        for i in range(9):
-            path_file = os.path.join(files_path, file)
-            print("yes, data : ", file)
-            data = unpickle(path_file)
-            plt.subplot(330 + 1 + i)
-            img = data[b'data'][i]
-            img_r = img[0:1024].reshape(32,32)
-            img_g = img[1024:2048].reshape(32,32)
-            img_b = img[2048:].reshape(32,32)
-            img_render = np.dstack((img_r, img_g, img_b))
-            plt.imshow(img_render)
-            print(i)
-        break
-
-plt.show()
+    plt.show()
