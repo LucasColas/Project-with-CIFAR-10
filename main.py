f20428deabca36f075babaaae8225882e8e5bec0
LucasColas <lucas.colas13@gmail.com>
2021-06-09 19:10:40 +0200

    update

--- main.py
+++ main.py
@@ -126,3 +126,10 @@ X_train, X_test, X_valid, Y_valid, X_test, Y_test = get_dataset(files_path, name
 model = models.Sequential()
 
 model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
+model.add(layers.MaxPooling2D(2,2))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.Conv2D(32, (3,3)))
+model.add(layers.MaxPooling2D(2,2))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D(2,2))
+model.add(layers.Conv2D(128, (3,3), activation="relu"))
