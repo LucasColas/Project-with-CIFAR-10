0900993f4ae86e9510a1bbfef7cdd067219e046a
LucasColas <lucas.colas13@gmail.com>
2021-06-13 06:03:39 +0200

    update

--- main.py
+++ main.py
@@ -152,5 +152,5 @@ model.add(layers.Dense(512, activation='relu'))
 model.add(layers.Dense(10,activation='softmax'))
 
 print(X_train.shape, X_valid.shape,X_test.shape )
-model.compile(optimizer=optimizers.RMSprop(lr=2e-5),loss="categorical_crossentropy", metrics=["acc"])
-history = model.fit(X_train, Y_train, batch_size=64, epochs=25, validation_data=(X_valid, Y_valid))
+model.compile(optimizer=optimizers.RMSprop(lr=2e-4),loss="categorical_crossentropy", metrics=["acc"])
+history = model.fit(X_train, Y_train, batch_size=64, epochs=30, validation_data=(X_valid, Y_valid))
