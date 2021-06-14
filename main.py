42994bf7a925fb0bfd762d45e6b0aa3c1eac1590
LucasColas <lucas.colas13@gmail.com>
2021-06-14 13:02:28 +0200

    update

--- main.py
+++ main.py
@@ -155,3 +155,25 @@ print(X_train.shape, X_valid.shape,X_test.shape )
 model.compile(optimizer=optimizers.RMSprop(lr=2e-4),loss="categorical_crossentropy", metrics=["acc"])
 history = model.fit(X_train, Y_train, batch_size=64, epochs=30, validation_data=(X_valid, Y_valid))
 model.save("training.h5")
+
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1,len(acc)+1)
+
+plt.plot(epochs,acc,'bo', label='Training acc')
+plt.plot(epochs, val_acc,'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
